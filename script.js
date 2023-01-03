067c698eb662441ec7d740885cf626e2b7879c4b
Rafal Glodek <rafal.glodek@interia.pl>
2023-01-03 06:13:51 +0000

    Api integration fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://rest-api-prod-kodilla-tasks-zsll1s.mo6.mogenius.io/';
-  const trelloApiRoot = 'https://trello.com/b/aAHylysh/kodilla-application';
+  //const trelloApiRoot = 'https://trello.com/b/aAHylysh/kodilla-application';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
